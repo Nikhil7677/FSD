bd0e903a9db8efb90adbc4abff3f4aeb44eb65ee
Nikhil7677 <mothukurinikhil2277@msitprogram.net>
2024-08-17 15:02:04 +0530

    Assignment_4

---
+++ Assignment_4.js
@@ -0,0 +1,11 @@
+for(let i = 0; i<=7;i++){
+    let hash = " "
+    for(let j = 0; j<=i; j++){
+        hash = hash + "#"
+    }
+    console.log(hash);
+    //console.log(hash.length)
+}
+
+let a = "abc"
+console.log(a.length)
